b01e7543fc5e9dc8852fc1467ace083ee8b10b6b
nortonrafli <norton.nra27@gmail.com>
2019-12-16 18:44:07 +0700

    Penambahan code fragment ,bookingfragment dan destinationfragment dan layout welcome slide 1 dan welcome slide 2

--- app/src/main/java/id/a/applikasibwi/Activity/NavigationActivity.java
+++ app/src/main/java/id/a/applikasibwi/Activity/NavigationActivity.java
@@ -1,16 +1,54 @@
 package id.a.applikasibwi.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import id.a.applikasibwi.Fragment.BookingFragment;
+import id.a.applikasibwi.Fragment.DestinationFragment;
 import id.a.applikasibwi.R;
 
 public class NavigationActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+        loadFragment(new DestinationFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.action_home:
+                fragment = new DestinationFragment();
+                break;
+            case R.id.action_download:
+                fragment = new BookingFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
